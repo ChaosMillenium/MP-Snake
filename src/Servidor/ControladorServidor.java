b0d0dd29a44c6d198a587673e0a3adb083f5670a
millenium <millenium@DESKTOP-GEDBTUJ>
2018-04-14 01:57:26 +0200

    Continuando el servidor

--- src/Servidor/ControladorServidor.java
+++ src/Servidor/ControladorServidor.java
@@ -5,7 +5,7 @@
  */
 package Servidor;
 
-import Utilidades.Coordenadas;
+import Utilidades.Direccion;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -20,35 +20,36 @@ public class ControladorServidor implements Observer {
     public ControladorServidor(ModeloJuego modelo){
         this.modelo=modelo;
     }
+    
     public void iniciarServer() {
         ThreadServer servidor = new ThreadServer(this);
         servidor.startServer();
     }
+    
     public int añadirJugador(){
         return this.modelo.añadirJugador();
     }
+    
     public int getFilas(){
         return this.modelo.getFilas();
     }
+    
     public int getColumnas(){
         return this.modelo.getColumnas();
     }
+    
     public int getTamañoBase(){
         return this.modelo.getTamañoBase();
     }
+    
     @Override
     public void update(Observable o, Object arg) {
 
+    }    
+    
+    public void cambiarDireccion(String direccion, int id) {
+        Direccion instanciaDir = Direccion.valueOf(direccion);
+        this.modelo.cambiarDireccion(instanciaDir,id);
     }
-
-    public int[] coordenadasIniciales() {
-         Coordenadas[] coordenadas = this.modelo.asignarCoordInicio();
-         int[] resultado = new int[this.getTamañoBase()*2];
-         for (int i = 0; i < resultado.length; i+=2){
-             resultado[i]=coordenadas[i].getX();
-             resultado[i+1]=coordenadas[i].getY();
-         }
-         return resultado;
-    }
-
+    
 }
--- src/Utilidades/ConstructorMensajes.java
+++ src/Utilidades/ConstructorMensajes.java
@@ -1,21 +1,27 @@
 package Utilidades;
 
 public class ConstructorMensajes{
+    
     public static String idc(int id){
         return "IDC;"+id;
     }
+    
     public static String tab(int filas,int columnas){
         return "TAB;"+filas+";"+columnas;
     }
+    
     public static String tsp(int tamaño){
         return "TSP;"+tamaño;
     }
+    
     public static String coi(int[] coordenadas){
         StringBuilder sb = new StringBuilder();
         sb.append("COI;");
-        for (int coordenada : coordenadas){
-            sb.append(coordenada).append(";");
+        for (int i = 0; i < coordenadas.length-1; i++){
+            sb.append(coordenadas[i]).append(";");
         }
+        sb.append(coordenadas[coordenadas.length-1]);
         return sb.toString();
     }
+    
 }
