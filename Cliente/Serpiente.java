7ff8d0568117b7948ead2c011b6fe95380eb933f
LordHansTopo <32304288+LordHansTopo@users.noreply.github.com>
2018-04-19 19:39:55 +0200

    Add files via upload

---
+++ Cliente/Serpiente.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente;
+
+import Utilidades.Coordenadas;
+import Utilidades.Direccion;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author danie
+ */
+public class Serpiente {
+    
+    private LinkedList<Coordenadas> serp;
+    private Direccion dir;
+    private Coordenadas cabeza;
+    private int id;
+    private long puntos;
+
+    public Serpiente(){
+        //Metodo constructor de serpiente
+        this.dir = Direccion.ARRIBA; //añadir direccion aleatoria en el futuro
+        this.serp = new LinkedList<Coordenadas>();
+        this.cabeza = coors.get(0);
+        for(int i = 0; i < coors.size(); i++){
+            this.serp.add(coors.get(i));
+        }
+        this.puntos = 0;
+    }
+    
+    
+
+    
+}
