9d1e7b864185c3ea2cf323649422fdc9b74f3905
LordHansTopo <danieldiz@hotmail.es>
2018-04-26 19:11:23 +0200

    sincronizados varias cosas
    correguido el envio de coordenadas por parte del servidor al cliente
    en cuanto se chocaba fuera del tablero
    
    errores a corregir: vista cliente no borra serpientes

--- src/Servidor/ControladorServidor.java
+++ src/Servidor/ControladorServidor.java
@@ -45,11 +45,11 @@ public class ControladorServidor implements Observer {
     public int getTamañoBase() {
         return this.modelo.getTamañoBase();
     }
-    
-    public Map<Integer, Jugador> getSerpientes(){
+
+    public Map<Integer, Jugador> getSerpientes() {
         return this.modelo.getJugadores();
     }
-    
+
     @Override
     public void update(Observable o, Object arg) {
         String accion = (String) arg;
@@ -66,41 +66,41 @@ public class ControladorServidor implements Observer {
                     this.modelo.getCabeza(id).getY()};
                 int[] cola = {this.modelo.getAnteriorCola(id).getX(),
                     this.modelo.getAnteriorCola(id).getY()};
-                this.servidor.moverJugador(id,cabeza,cola);
+                this.servidor.moverJugador(id, cabeza, cola);
                 break;
             }
-            
+
             case "COL": {
                 int id1 = Integer.parseInt(parseado[1]);
                 int id2 = Integer.parseInt(parseado[2]);
                 this.servidor.colision(id1, id2);
                 break;
             }
-            
+
             case "CBR": {
                 int id = Integer.parseInt(parseado[1]);
                 this.servidor.colision(id);
                 break;
             }
-            
+
             case "PTS": {
                 int id = Integer.parseInt(parseado[1]);
                 int puntos = Integer.parseInt(parseado[2]);
                 this.servidor.darPuntos(id, puntos);
                 break;
             }
-            
+
             case "TSR": {
                 int x = Integer.parseInt(parseado[1]);
                 int y = Integer.parseInt(parseado[2]);
-                this.servidor.nuevoTesoro(x,y);
+                this.servidor.nuevoTesoro(x, y);
                 break;
             }
-            default:{
+            default: {
                 System.err.println(accion);
                 break;
             }
-            
+
         }
 
     }
@@ -116,6 +116,18 @@ public class ControladorServidor implements Observer {
 
     public void eliminarJugador(int id) {
         this.modelo.eliminarJugador(id);
+    }
+
+    public int[] getCoordenadasAnt(int id) {
+        Coordenadas[] coordenadas = this.modelo.getCoordenadasAnt(id);
+        int[] coordInt = new int[coordenadas.length * 2];
+        int j = 0;
+        for (int i = 0; i < coordInt.length; i += 2) {
+            coordInt[i] = coordenadas[j].getX();
+            coordInt[i + 1] = coordenadas[j].getY();
+            j++;
+        }
+        return coordInt;
     }
 
     public int[] getCoordenadas(int id) {
@@ -134,7 +146,7 @@ public class ControladorServidor implements Observer {
         return this.modelo.siguienteKey();
     }
 
-    public List<Coordenadas> getTesoros(){
+    public List<Coordenadas> getTesoros() {
         return this.modelo.getTesoros();
     }
 
--- src/Servidor/ThreadActualizarTablero.java
+++ src/Servidor/ThreadActualizarTablero.java
@@ -5,6 +5,7 @@
  */
 package Servidor;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Random;
 
@@ -15,7 +16,6 @@ import java.util.Random;
 public class ThreadActualizarTablero extends Thread {
 
     private ModeloJuego modelo;
-    private Map<Integer, Jugador> jugadores;
     private boolean pausa = false;
     private final int VELOCIDAD;
     private final int PROBABILIDAD = 1;
@@ -23,31 +23,34 @@ public class ThreadActualizarTablero extends Thread {
     public ThreadActualizarTablero(ModeloJuego modelo) {
         this.modelo = modelo;
         this.VELOCIDAD = this.modelo.getVELOCIDAD();
-        this.jugadores = this.modelo.getJugadores();
     }
 
     @Override
     public void run() {
         Random aleatTesoro = new Random();
         while (hayJugadores()) {
-                while (this.pausa) {
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        System.err.println("Error en hilo de tablero.");
-                        this.start();
-                    }
+            while (this.pausa) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    System.err.println("Error en hilo de tablero.");
+                    this.start();
                 }
-                for (Map.Entry<Integer, Jugador> entrada : this.jugadores.entrySet()) { //Movimiento de jugadores
+            }
+            synchronized (this.modelo.getJugadores()) {
+                for (Map.Entry<Integer, Jugador> entrada : this.modelo.getJugadores().entrySet()) { //Movimiento de jugadores
                     entrada.getValue().nuevaCabeza();
                     entrada.getValue().eliminarCola();
                     this.modelo.notificarMovimiento(entrada.getKey());
                     entrada.getValue().setEspera(false); //Ya se puede cambiar de dirección
                 }
+            }
+            synchronized (this.modelo.getTesoros()) {
                 //TODO: Revisar probabilidades
                 if (this.modelo.getTesoros().isEmpty() || (aleatTesoro.nextInt(100) < PROBABILIDAD)) { //PROBABILIDAD % cada VELOCIDAD ms de generar un tesoro
                     this.modelo.generarTesoro();
-                }           
+                }
+            }
             try {
                 Thread.sleep(this.VELOCIDAD);
             } catch (InterruptedException e) {
